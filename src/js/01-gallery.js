3ad792443b844037e8dc07035582de556ee34261
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-09 15:21:02 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) =>
+`<a class="gallery__item" href = ${original}><img class="gallery__image" src = ${preview} alt
+ = ${description}/></a>`).join("");
+
+imageContainer.insertAdjacentHTML("afterbegin", markup);
+// imageContainer.addEventListener('click', changeCrsImages);
+
+     
+let gallery = new SimpleLightbox('.gallery a', {
+    overlayOpasity: 0.8,
+    fadeSpeed: 250,
+    captionsData: 'alt',
+    
+});
+  
+gallery.on('show.SimpleLightbox', function (e) {
+    sourceAttr: 'href';
+});
