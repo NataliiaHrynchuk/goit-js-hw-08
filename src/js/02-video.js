d02da9c76e996e682de4814447f261171750f510
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-22 13:40:31 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from "@vimeo/player";
+var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 
@@ -11,7 +12,7 @@ const iframe = document.querySelector('iframe');
         localStorage.setItem('videoplayer-current-time', pausedTime);
      };
 
-    player.on('timeupdate', onPlay);
+    player.on('timeupdate', throttle(onPlay, 1000));
 
     resultTime = localStorage.getItem('videoplayer-current-time');
     
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+var throttle = require('lodash.throttle');
 
+const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+ 
+
+let feedbackObject = {
+    email: '',
+    message: '',
+};
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(feedbackObject);
+    refs.form.reset();
+    localStorage.clear();
+};
+
+ function handleInputEmail(event) {
+    event.preventDefault();
+    const email = event.target.value;
+    feedbackObject.email = email;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObject));
+ };
+
+ function handleFocusEmail(event) {
+    event.target.value = feedbackObject.email;
+};
+
+ function handleInputMessage(event) {
+    event.preventDefault();
+    const message = event.target.value;
+    feedbackObject.message = message;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObject));
+ };
+
+ function handleFocusMessage(event) {
+    event.target.value = feedbackObject.message;
+};
+
+ 
+refs.input.addEventListener('input', throttle(handleInputEmail, 500));
+refs.input.addEventListener('focus', handleFocusEmail);
+refs.textarea.addEventListener('input', throttle(handleInputMessage, 500));
+refs.textarea.addEventListener('focus', handleFocusMessage);
+refs.form.addEventListener('submit', onFormSubmit);
