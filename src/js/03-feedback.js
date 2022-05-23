48f1e539d4e7f2604d960b021e540a8b42363c4c
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-23 23:00:52 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,48 @@
 var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
 const refs = {
     form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('textarea'),
+    };
+
+    refs.form.addEventListener('submit', onFormSubmit);
+    refs.form.addEventListener('input', throttle(onFormInput, 500));
+    loadData();
+
+    const formData = {};
+const saveData = () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    // console.log(formData);
 };
- 
 
-let feedbackObject = {
-    email: '',
-    message: '',
+function loadData() {
+    const feedbackObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (feedbackObject) {
+        refs.form.elements.email.value = feedbackObject.email || '';
+        refs.form.elements.message.value = feedbackObject.message || '';
+    }
 };
 
-
-function onFormSubmit(event) {
+ function onFormInput(event) {
     event.preventDefault();
+    
+    formData[event.target.name] = event.target.value;
 
-    console.log(feedbackObject);
+    saveData();
+    // const email = refs.form.elements.email.value;
+    // const message = refs.form.elements.message.value;
+};
+
+ function onFormSubmit(event) {
+    event.preventDefault();
+   
     refs.form.reset();
-    localStorage.clear();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
 };
 
- function handleInputEmail(event) {
-    event.preventDefault();
-    const email = event.target.value;
-    feedbackObject.email = email;
-    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObject));
- };
 
- function handleFocusEmail(event) {
-    event.target.value = feedbackObject.email;
-};
 
- function handleInputMessage(event) {
-    event.preventDefault();
-    const message = event.target.value;
-    feedbackObject.message = message;
-    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObject));
- };
 
- function handleFocusMessage(event) {
-    event.target.value = feedbackObject.message;
-};
 
- 
-refs.input.addEventListener('input', throttle(handleInputEmail, 500));
-refs.input.addEventListener('focus', handleFocusEmail);
-refs.textarea.addEventListener('input', throttle(handleInputMessage, 500));
-refs.textarea.addEventListener('focus', handleFocusMessage);
-refs.form.addEventListener('submit', onFormSubmit);
+
